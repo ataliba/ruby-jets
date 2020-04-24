a6c653f99b6cbf6425eb7f1ba7013b7507ab486f
Ataliba Teixeira <ataliba@pm.me>
2020-04-24 19:43:10 -0300

    changes

---
+++ ls.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#===============================================================================
+#
+#          FILE:  ls.sh
+# 
+#         USAGE:  ./ls.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:   (), 
+#       COMPANY:  
+#       VERSION:  1.0
+#       CREATED:  04/24/2020 07:32:45 PM -03
+#      REVISION:  ---
+#===============================================================================
+
+echo "ls no root" 
+
+ls -lah 
+
+
+echo "ls no /root" 
+
+ls -lah /root 
+
+
